9ea1cbd24509fb662bdad8aa0f02837e5ac46351
LuzPaty <luzpatyhr@gmail.com>
2025-03-12 16:52:38 -0600

    pckg

--- HelloWorld-java/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ HelloWorld-java/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,5 +1,5 @@
 //This file starts the Spring Boot application.
-
+package com.example.helloworld; 
 import org.springframework.boot.SpringApplication; //used to start the Spring Boot application
 import org.springframework.boot.autoconfigure.SpringBootApplication; //used to enable auto-configuration and component scanning
 
